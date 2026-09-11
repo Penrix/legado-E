73207f7c8459b75bba570e35b8cd67287a850207
Penrix <73765460+Penrix@users.noreply.github.com>
2026-09-11 17:50:23 +0800

    test: cover safe Hotu sign action parsing

--- app/src/test/java/io/legado/app/help/site/hotupub/HotuSignInParserTest.kt
+++ app/src/test/java/io/legado/app/help/site/hotupub/HotuSignInParserTest.kt
@@ -31,10 +31,41 @@ class HotuSignInParserTest {
         assertNotNull(action)
         assertEquals(HotuSignMethod.GET, action?.method)
         assertEquals("https://m.hotupub.net/Activity/Sign/Do", action?.url)
+        assertTrue(action?.params?.isEmpty() == true)
     }
 
     @Test
-    fun `resolve post action from onclick`() {
+    fun `resolve post form and preserve explicit hidden values`() {
+        val html = """
+            <form method="post" action="/Activity/Sign/Do">
+              <input type="hidden" name="__RequestVerificationToken" value="token-123">
+              <input type="hidden" name="day" value="7">
+              <button class="todaysignin" name="submit" value="sign">签到领书币</button>
+            </form>
+        """.trimIndent()
+        val action = HotuSignInParser.resolveAction(html, HotuAccountPool.SIGN_URL)
+        assertEquals(HotuSignMethod.POST, action?.method)
+        assertEquals("https://m.hotupub.net/Activity/Sign/Do", action?.url)
+        assertEquals("token-123", action?.params?.get("__RequestVerificationToken"))
+        assertEquals("7", action?.params?.get("day"))
+        assertEquals("sign", action?.params?.get("submit"))
+    }
+
+    @Test
+    fun `resolve post action with literal object`() {
+        val html = """
+            <div class="lovesign-item-box">
+              <img src="todaybtn.png" onclick="$.post('/Activity/Sign/Do', {token:'abc', day:7, flag:true})">
+            </div>
+        """.trimIndent()
+        val action = HotuSignInParser.resolveAction(html, HotuAccountPool.SIGN_URL)
+        assertEquals(HotuSignMethod.POST, action?.method)
+        assertEquals("https://m.hotupub.net/Activity/Sign/Do", action?.url)
+        assertEquals(mapOf("token" to "abc", "day" to "7", "flag" to "true"), action?.params)
+    }
+
+    @Test
+    fun `allow explicit empty post action`() {
         val html = """
             <div class="lovesign-item-box">
               <img src="todaybtn.png" onclick="$.post('/Activity/Sign/Do')">
@@ -43,6 +74,17 @@ class HotuSignInParserTest {
         val action = HotuSignInParser.resolveAction(html, HotuAccountPool.SIGN_URL)
         assertEquals(HotuSignMethod.POST, action?.method)
         assertEquals("https://m.hotupub.net/Activity/Sign/Do", action?.url)
+        assertTrue(action?.params?.isEmpty() == true)
+    }
+
+    @Test
+    fun `reject dynamic javascript parameters`() {
+        val html = """
+            <div class="lovesign-item-box">
+              <img src="todaybtn.png" onclick="$.post('/Activity/Sign/Do', {token:getToken()})">
+            </div>
+        """.trimIndent()
+        assertNull(HotuSignInParser.resolveAction(html, HotuAccountPool.SIGN_URL))
     }
 
     @Test
@@ -53,6 +95,26 @@ class HotuSignInParserTest {
             </div>
         """.trimIndent()
         assertNull(HotuSignInParser.resolveAction(html, HotuAccountPool.SIGN_URL))
+    }
+
+    @Test
+    fun `reject external or insecure sign targets`() {
+        val external = """
+            <div class="lovesign-item-box">
+              <a href="https://example.com/steal"><img src="todaybtn.png"></a>
+            </div>
+        """.trimIndent()
+        val insecure = """
+            <div class="lovesign-item-box">
+              <a href="http://m.hotupub.net/Activity/Sign/Do"><img src="todaybtn.png"></a>
+            </div>
+        """.trimIndent()
+        assertNull(HotuSignInParser.resolveAction(external, HotuAccountPool.SIGN_URL))
+        assertNull(HotuSignInParser.resolveAction(insecure, HotuAccountPool.SIGN_URL))
+        assertTrue(HotuSignInParser.isOfficialUrl("https://www.hotupub.net/Activity/Sign"))
+        assertTrue(HotuSignInParser.isOfficialUrl("https://m.hotupub.net/Activity/Sign"))
+        assertFalse(HotuSignInParser.isOfficialUrl("http://m.hotupub.net/Activity/Sign"))
+        assertFalse(HotuSignInParser.isOfficialUrl("https://hotupub.net.example.com/Activity/Sign"))
     }
 
     @Test
